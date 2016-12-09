662c3d903fcbdea6f8153c3cb235eb94b2ea1987
USER <USER>
2016-12-09 07:45:00 +0100

    working on tests, jsdom problem with querySelector?

--- tests/index.js
+++ tests/index.js
@@ -6,7 +6,7 @@ const test = require('tape'),
     TEST_SERVER = 'http://localhost:' + PORT + '/';
 
 function router(req, res) {
-    if(req.url.indexOf('refresh.svg') > -1) {
+    if (req.url.indexOf('refresh.svg') > -1) {
         res.writeHead(200, {'Content-Type': 'image/svg+xml'});
         res.write(fs.readFileSync(__dirname + '/../refresh.svg'));
     } else {
@@ -16,20 +16,88 @@ function router(req, res) {
     res.end();
 }
 
-http.createServer(router).listen(PORT, function () {
+var testServer = http.createServer(router),
+    localStorage = {
+        __store: {},
+        setItem: function (name, obj) {
+            localStorage.__store[name] = obj;
+        },
+        getItem: function (name) {
+            return localStorage.__store[name];
+        }
+    };
+
+function getDom(cb) {
+    jsdom.env({
+        url: TEST_SERVER,
+        features: {
+            FetchExternalResources: ['script'],
+            ProcessExternalResources: ['script'],
+            QuerySelector: true
+        },
+        done: function (err, window) {
+            if (cb) cb(err, window);
+        }
+    });
+}
+
+testServer.listen(PORT, function () {
     test('site displays', function (t) {
-        jsdom.env(TEST_SERVER, function (err, window) {
+        getDom(function (err, window) {
+            t.plan(1);
             if (err) t.fail('Error parsing website');
 
-            t.ok(window.document.getElementById('score-board'), 'Score board not visible');
+            t.ok(window.document.getElementById('score-board'), 'Score board is visible');
         })
     });
 
-    test('diagonal combo', function (t) {
-        t.fail('TODO');
+    test('can click on cell', function (t) {
+        t.plan(1);
+        t.fail('TODO')
+    });
+
+/*    test('can get cell by coord', function (t) {
+        t.plan(1);
+        getDom(function (err, window) {
+            t.equals(window.getCellByCoord({
+                x: 2,
+                y: 3
+            }).dataset.x, 2, 'getCellByCoord should return correct coord cell');
+        })
+    });
+    */
+
+    test('load previous game with diagonal combo', function (t) {
+        t.plan(3);
+
+        getDom(function (err, window) {
+            if (!window.localStorage) window.localStorage = localStorage;
+
+            if (err) t.fail('Error parsing website');
+
+            t.equals(typeof window.loadPreviousGame, 'function', 'loadPreviousGame() should be a function');
+
+            window.localStorage.setItem('endlessGomoku', JSON.stringify([
+                {player: 'x', coord: {x: 10, y: 25}},
+                {player: 'x', coord: {x: 11, y: 24}},
+                {player: 'x', coord: {x: 12, y: 23}},
+                {player: 'x', coord: {x: 13, y: 22}},
+                {player: 'x', coord: {x: 14, y: 21}}
+            ]));
+            
+            window.loadPreviousGame();
+
+            var oneOfComboCells = window.getCellByCoord({x: 12, y: 23});
+            t.ok(oneOfComboCells, 'Combo cell should exist at those coordinates');
+
+            t.equals(oneOfComboCells.dataset.combotype, 'northwest', 'Combo type should be northwest');
+        });
+
+
     });
 
     test.onFinish(function () {
-       http.close();
+        testServer.close();
+        process.exit();
     });
 });
